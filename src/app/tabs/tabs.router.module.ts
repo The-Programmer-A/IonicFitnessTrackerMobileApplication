7b8e897e30375562fa86308d6dcf8b908f6ab3ab
Armaan Chandra <chandrarma@ecs.vuw.ac.nz>
2019-07-22 13:56:01 +1200

    added a tabs page that houses the 2 main components of the actual application. Going to work on the calendar next

---
+++ src/app/tabs/tabs.router.module.ts
@@ -0,0 +1,23 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+	{
+		path: '', //default path
+		component: TabsPage, //spesifies which componenet of the tabs we want the path to point to 
+		children: [ //these are the components options
+            { path: 'calendar', loadChildren: '../calendar/calendar.module#CalendarPageModule' },
+            { path: 'new-record', loadChildren: '../new-record/new-record.module#NewRecordPageModule' },
+		]
+	}	
+];
+
+@NgModule({
+    imports: [
+      RouterModule.forChild(routes)
+    ],
+    exports: [RouterModule]
+  })
+  export class TabsRoutingModule { }
+  
